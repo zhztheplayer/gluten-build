202a8929b32f8829ea56f3f259688fcb5bcec60e
Hongze Zhang <hongze.zhang@intel.com>
2022-11-01 16:43:32 +0800

    Support backend gazelle-cpp

--- cmvn.sh
+++ cmvn.sh
@@ -6,6 +6,6 @@ BASEDIR=$(dirname $0)
 
 CMVN_CMD_ARGS="$*"
 MAVEN_ARGS="$CMVN_CMD_ARGS"
-BASH_ARGS="cd /opt/gluten && mvn \$GLUTEN_MAVEN_OPTIONS $MAVEN_ARGS"
+BASH_ARGS="cd /opt/gluten && mvn $MAVEN_ARGS"
 
 $BASEDIR/cbash.sh "$BASH_ARGS"
